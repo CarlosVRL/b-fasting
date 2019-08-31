3ae15a6a9ecb02bb82b6a28834eaf628eecfdf00
CarlosVRL <carlos.varela.eng@gmail.com>
2019-08-30 22:41:51 -0500

    Updates

---
+++ precache-manifest.157a9b079e8ac1032dea704174a31175.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185fa5137a3949d0ad28e82732098d0f",
+    "url": "/b-fasting/index.html"
+  },
+  {
+    "revision": "4c824c85f5ecbb15eefc",
+    "url": "/b-fasting/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "582d6872bdbbe5f9b853",
+    "url": "/b-fasting/static/js/2.38ab91f3.chunk.js"
+  },
+  {
+    "revision": "4c824c85f5ecbb15eefc",
+    "url": "/b-fasting/static/js/main.40e72ad2.chunk.js"
+  },
+  {
+    "revision": "fa598a2e5f522ae0980f",
+    "url": "/b-fasting/static/js/runtime~main.b5e20702.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/b-fasting/static/media/logo.5d5d9eef.svg"
+  }
+]);
