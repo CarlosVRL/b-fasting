3d85d05ae819248f1cb44732f86ebaa0ca183769
CarlosVRL <carlos.varela.eng@gmail.com>
2019-08-30 23:05:53 -0500

    Migrate initial Be Fasting capability from codepen

--- src/App.js
+++ src/App.js
@@ -4,23 +4,80 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-wrapper">
+        <Header />
+        <Timeset />
+        <Timer />
     </div>
   );
+}
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="header-container">
+        <h1 className="header-content">Be Fasting</h1>
+      </div>
+    );
+  }
+}
+
+
+class Timeset extends React.Component {
+  render() {
+    return (
+      <div className="timeset-container">
+        <img
+          className="timeset-img"
+          src="https://images.unsplash.com/photo-1418848372148-629424a5ca04?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="set the time"
+        />
+      </div>
+    );
+  }
+}
+
+class Inspiration extends React.Component {
+  renter() {
+    return (
+      <div className="inspiration-container">
+        <img
+          className="inspiration-img"
+          src="https://images.unsplash.com/photo-1501004318641-b39e6451bec6?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
+          alt="don't give up"
+        />
+      </div>
+    );
+  }
+}
+
+class Timer extends React.Component {
+  handleStart() {
+    alert('Coming Soon!');
+  }
+  render() {
+    return (
+      <div className="timer-container">
+        <label for="business">TIME TO FAST</label>
+        <select className="timer-select" name="business">
+          <option value="select">What is your goal?</option>
+          <option value="small">16 Hours</option>
+          <option value="medium">24 Hours</option>
+          <option value="large">48 Hours</option>
+        </select>
+        <button
+          id="timer-start-btn"
+          type="button"
+          onClick={this.handleStart}
+        >
+          Start!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
